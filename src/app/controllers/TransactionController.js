3730237df0ffe1f6f3d126ac8a9dcfc33f873e0b
Carlos Eduardo <carloseduardo.and2@gmail.com>
2022-01-06 22:31:50 -0300

    feat: implement put operation

--- src/app/controllers/TransactionController.js
+++ src/app/controllers/TransactionController.js
@@ -37,6 +37,37 @@ class TransactionController {
     response.status(201).json(transaction);
   }
 
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      description, amount, date,
+    } = request.body;
+
+    if (!description) {
+      return response.status(400).json({
+        error: 'Description is required',
+      });
+    }
+
+    if (!amount) {
+      return response.status(400).json({
+        error: 'Amount is required',
+      });
+    }
+
+    if (!date) {
+      return response.status(400).json({
+        error: 'Date is required',
+      });
+    }
+
+    const transaction = await TransactionsRepository.update(id, {
+      description, amount, date,
+    });
+
+    response.json(transaction);
+  }
+
   async delete(request, response) {
     const { id } = request.params;
 
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.get('/transactions', TransactionController.index);
 router.post('/transactions', TransactionController.store);
+router.put('/transactions/:id', TransactionController.update);
 router.delete('/transactions/:id', TransactionController.delete);
 
 module.exports = router;
