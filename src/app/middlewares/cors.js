2ab48e9b904e4a7d57b18803a859a0080c550564
Carlos Eduardo <carloseduardo.and2@gmail.com>
2022-03-23 22:35:35 -0300

    refactor: change cors middleware

--- src/app/middlewares/cors.js
+++
@@ -1,5 +0,0 @@
-module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 require('express-async-errors');
 
-const cors = require('./app/middlewares/cors');
+const cors = require('cors');
 const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.use(routes);
 app.use(errorHandler);
 
